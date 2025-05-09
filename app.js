2dc1354ee628175f0a7132ec7a6a38f7fa6134e0
tejas-jadhav-cse <tejas.jadhav.cse@gmail.com>
2025-05-09 12:41:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ document.addEventListener('keydown', function(e) {
 
 // Cursor object with all cursor functionality
 const cursor = {
-    delay: 5,
+    delay: 8,
     _x: 0,
     _y: 0,
     endX: window.innerWidth / 2,
